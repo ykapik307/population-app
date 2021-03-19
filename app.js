9ce0ebf3f2905f3ab39496d8bf037b66744b63c1
ykapik307 <47310544+ykapik307@users.noreply.github.com>
2021-03-19 14:56:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
-let cName = document.querySelector('.cn');
-let subButton = document.querySelector('.sub');
-let pop = document.querySelector('.res-pop');
-let rank = document.querySelector('.ranking-pop');
-let share = document.querySelector('.worldShare');
+
+function myFunction()
+{
+    let x= document.getElementById("myform").elements[0].value;
+   let c=0;
+   let k=0;
+let y=document.getElementById("myform").elements[1].value;
+let z=document.getElementById("myform").elements[2].value;
+if(y=="Outstanding")
+{
+    c=0.30;
+}
+else if(y=="Good")
+{
+    c=0.20;
+}
+else if(y=="Bad")
+{
+    c=0.10;
+}
+else if(y=="Terrible"){
+    c=0.05;
+}
+else{
+    c=0.15;
+}
+k=(x*c)/z;
+let n = k.toFixed(2);
+let bill1=x/z;
+bill=bill1.toFixed(2);
+
+let tot1=parseFloat(bill)+parseFloat(n);
+let tot=tot1.toFixed(2);
+document.getElementById("tip").innerHTML='Bill Amount : ₹'+bill+' each';
+document.getElementById("demo").innerHTML='Tip Amount : ₹'+n+' each';
+document.getElementById("total").innerHTML='Total Amount : ₹'+tot+' each';
 
 
-
-subButton.addEventListener('click', () => {
-
-    let country = cName.value;
-
-let fCountry = capital(country);
-function capital(input) {  
-    var words = input.split(' ');  
-    var CapitalizedWords = [];  
-    words.forEach(element => {  
-        CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
-    });  
-    return CapitalizedWords.join(' ');  
-} 
-console.log(fCountry);
-   
-    fetch(`https://world-population.p.rapidapi.com/population?country_name=${fCountry}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "a1c59c46bdmshda8f76ca21bb37cp19e40ajsn8a03a6fec378",
-            "x-rapidapi-host": "world-population.p.rapidapi.com"
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            let x=data.body.population;
-            let num=thousands_separators(x);
-            pop.innerHTML="Population : "+num;
-            rank.innerHTML="World Rank : "+data.body.ranking;
-            share.innerHTML="Global Share : "+data.body.world_share.toFixed(2)+"%";
-        });
-
-})
-function thousands_separators(num)
-  {
-    var num_parts = num.toString().split(".");
-    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return num_parts.join(".");
-  }
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+}
